836c57082168df67298e5bf7824b18a22d51fe92
Huan Barros <audax.py@gmail.com>
2022-12-08 09:15:53 -0400

    update

---
+++ 001-forca/src/forca.py
@@ -0,0 +1,70 @@
+
+import random as rd
+from pathlib import Path 
+
+# Escolhendo o assunto
+print("""
+Escolha um assunto para jogar
+
+[0] Animais
+[1] Frutas
+""")
+
+escolha_usr = int(input('Digite um numero correspondete a um assunto: '))
+lista_assuntos = ['animais', 'frutas']
+assunto = lista_assuntos[escolha_usr]
+
+
+# Escolhendo a palavra relacionada ao assunto
+with open(file=f'{Path().absolute()}/database/{assunto}.txt', 
+          mode='r', 
+          encoding='utf-8') as arquivo:
+    
+    lista_palavras = arquivo.readlines()
+
+palavra_escolhida = rd.sample(lista_palavras, 1)[0].strip('\n')
+
+
+# Construindo o loop
+palavra = ['_' for letra in palavra_escolhida]
+vida = 3
+tentativas_usr = []
+
+while True:
+
+    print(f'\nA palavra escolhida é: {palavra}')
+
+    if '_' not in palavra:
+        print('\nPARABÉNS, VOCÊ GANHOU\n')
+        break
+
+    letra_usr = input('Digite uma letra: ')
+
+    if not(letra_usr.isalpha()):
+        print('\nERRO, DIGITE APENAS LETRAS')
+        continue
+
+    if len(letra_usr) > 1:
+        print('\nERRO, DIGITE APENAS UMA LETRA')
+        continue
+
+    if letra_usr in tentativas_usr:
+        print('\nVOCÊ JÁ DIGITOU ESSA LETRA, TENTE OUTRA')
+        continue
+    else:
+        tentativas_usr.append(letra_usr)
+
+    if letra_usr in palavra_escolhida:
+        for pos, letra in enumerate(palavra_escolhida):
+            if letra_usr == letra:
+                palavra[pos] = letra_usr
+
+    else:
+        vida-=1
+
+        if vida == 0:
+            print('\nVOCÊ PERDEU TODAS AS VIDAS, GAME OVER')
+            break
+
+        print(f'\nVOCÊ ERROU E AGORA SÓ TEM {vida} VIDA')
+
---
+++ 001-forca/src/teste.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+assunto = 'animais'
+
+print(f'{Path().absolute()}')
